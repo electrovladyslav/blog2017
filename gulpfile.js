94e867f45cb581e72b0176c9a32e0c8bba03bd9f
Yermolin Vlad <electrovladyslav@gmail.com>
2017-04-17 21:39:36 +0700

    Fonts were changed

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var minify = require("gulp-csso");
 var rename = require("gulp-rename");
 var server = require("browser-sync").create();
 var run = require("run-sequence");
+var del = require("del");
+var ghPages = require('gulp-gh-pages');
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -38,4 +40,34 @@ gulp.task("serve", ["style"], function() {
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
   gulp.watch("js/*.js").on("change", server.reload);
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "css/**",
+    "fonts/**",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", function (fn) {
+  run (
+    "clean",
+    "copy",
+    fn
+  );
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./build/**/*')
+    .pipe(ghPages());
 });
---
+++ js/map-marinetraffic.js
@@ -0,0 +1,11 @@
+width='100%';	// the width of the embedded map in pixels or percentage
+height='450';	// the height of the embedded map in pixels or percentage
+border='0';	// the width of the border around the map (zero means no border)
+shownames='true';	// to display ship names on the map (true or false)
+//latitude='0';	// the latitude of the center of the map, in decimal degrees
+//longitude='75';	// the longitude of the center of the map, in decimal degrees
+//zoom='9';	// the zoom level of the map (values between 2 and 17)
+// maptype='1';	// use 0 for Normal Map, 1 for Satellite, 2 for OpenStreetMap
+trackvessel='229441000';	// MMSI of a vessel (note: vessel will be displayed only if within range of the system) - overrides "zoom" option
+fleet='lgteisgk@yomail.info';	// the registered email address of a user-defined fleet (user's default fleet is used)
+//Read more at http://www.marinetraffic.com/en/p/embed-map#58LvjpDLlTdbxHYR.99
