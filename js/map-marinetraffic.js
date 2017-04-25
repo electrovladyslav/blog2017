9bdb0f5cf7f558a59750be9b37b24e6efc05a4ef
Yermolin Vlad <electrovladyslav@gmail.com>
2017-04-25 23:54:48 +0530

    Update 2017-04-25T18:24:41.778Z

--- js/map-marinetraffic.js
+++ js/map-marinetraffic.js
@@ -6,6 +6,6 @@ shownames='true';	// to display ship names on the map (true or false)
 //longitude='75';	// the longitude of the center of the map, in decimal degrees
 //zoom='17';	// the zoom level of the map (values between 2 and 17)
 // maptype='1';	// use 0 for Normal Map, 1 for Satellite, 2 for OpenStreetMap
-trackvessel='0' //'229441000';	// MMSI of a vessel (note: vessel will be displayed only if within range of the system) - overrides "zoom" option
+trackvessel='229441000';	// MMSI of a vessel (note: vessel will be displayed only if within range of the system) - overrides "zoom" option
 fleet='lgteisgk@yomail.info';	// the registered email address of a user-defined fleet (user's default fleet is used)
 //Read more at http://www.marinetraffic.com/en/p/embed-map#58LvjpDLlTdbxHYR.99
